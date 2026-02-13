74019b499b7e103569eca756b72d1fbb8ce3e74a
goharZakaryan <zakaryana264@gmail.com>
2026-02-12 23:24:59 -0800

    music upload

---
+++ streamus-server/src/main/java/com/example/streamusserver/post/dto/response/SongResponseDto.java
@@ -0,0 +1,49 @@
+package com.example.streamusserver.post.dto.response;
+
+public class SongResponseDto {
+    public String name;
+    public String songUrl;
+    public String albumArtUrl;
+    public int duration;
+    public int size;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSongUrl() {
+        return songUrl;
+    }
+
+    public void setSongUrl(String songUrl) {
+        this.songUrl = songUrl;
+    }
+
+    public String getAlbumArtUrl() {
+        return albumArtUrl;
+    }
+
+    public void setAlbumArtUrl(String albumArtUrl) {
+        this.albumArtUrl = albumArtUrl;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
